84cef7ac948d944b5562024f9abe76d113b7c05f
chunranchen <159973185+chunranchen@users.noreply.github.com>
2024-05-13 00:53:05 +0000

    commit

--- scripts/main.py
+++ scripts/main.py
@@ -5,4 +5,7 @@ config_file_path = "config/config.json"
 with open(config_file_path,'r') as config_file:
     config = json.load(config_file)
 
-Connection_STRING = config["connectionString"]
+ConnectionString = config["connectionString"]
+print(ConnectionString)
+
+
